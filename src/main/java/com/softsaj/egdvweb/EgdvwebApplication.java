1412310a1de1d09f928c6070f85b02403b4dc28e
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-06-06 05:14:14 -0400

    first commit

---
+++ src/main/java/com/softsaj/egdvweb/EgdvwebApplication.java
@@ -0,0 +1,13 @@
+package com.softsaj.egdvweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgdvwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgdvwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softsaj/egdvweb/EgdvwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softsaj.egdvweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgdvwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
